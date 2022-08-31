3756a74a9797296b8517a13bab9134c5e6bfbf28
andrejarboe <andrejarboe@gmail.com>
2022-08-31 16:57:19 -0400

    canDrive conditions

---
+++ basicsPart2/canDrive.py
@@ -0,0 +1,10 @@
+isOld = True
+isLicensed = True
+
+if isOld and isLicensed:
+    print('You are old enough to drive!')
+elif isLicensed:
+    print('You can drive now!')
+else:
+    print('You are not of age...')
+
