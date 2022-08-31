e2080593fcbf338760900a04cbe08096c0012f80
andrejarboe <andrejarboe@gmail.com>
2022-08-31 17:04:00 -0400

    #Truthy and Falsy

--- basicsPart2/canDrive.py
+++ basicsPart2/canDrive.py
@@ -2,9 +2,15 @@ isOld = True
 isLicensed = True
 
 if isOld and isLicensed:
-    print('You are old enough to drive!')
+    print('You are old enough to drive!, and you have a license')
 elif isLicensed:
     print('You can drive now!')
 else:
     print('You are not of age...')
 
+#Truthy and Falsy
+
+#true, true, false
+print(bool('hello'))
+print(bool(5))
+print(bool(0))
