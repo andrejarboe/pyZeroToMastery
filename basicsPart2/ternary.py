04e9ccec09799d612ffa214646706775e93a2154
andrejarboe <andrejarboe@gmail.com>
2022-08-31 17:06:33 -0400

    Ternary Operator

---
+++ basicsPart2/ternary.py
@@ -0,0 +1,9 @@
+#Ternary Operator
+
+# condition_if_true if condition else condition_if_false
+
+isFriend = True
+
+canMessage = 'message allowed' if isFriend else 'not allows to message'
+
+print(canMessage)
