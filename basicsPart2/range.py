e21c0c7c479e105e082f15d336afbd0786b067b5
andrejarboe <andrejarboe@gmail.com>
2022-08-31 17:43:30 -0400

    make range

---
+++ basicsPart2/range.py
@@ -0,0 +1,48 @@
+print(range(100))
+print(list(range(100)))
+
+
+
+# 1
+# 2
+# 3
+# 4
+for _ in range(1,5):
+    print(_)
+
+print("\n")
+
+#range
+# arg1 = start 
+# arg2 = end
+# arg3 = step over
+# 1
+# 3
+# 5
+# 7
+# 9
+for _  in range(1,10,2):
+    print(_)
+
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+print("\n")    
+for _ in range(10,0,-1):
+    print(_)
+
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print("\n")    
+for _ in range(5):
+    print(list(range(10)))
