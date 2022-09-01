253c24abbebf4d598b248174753bde7a64f0793d
Andre Jarboe <andrejarboe@gmail.com>
2022-09-01 16:40:53 -0400

    bool

---
+++ basicsPart1/booleans.py
@@ -0,0 +1,14 @@
+name = 'Andre'
+is_cool = False
+is_cool = True
+
+print(bool(-0))
+print(bool(0))
+print(bool(0.0))
+print(bool(1))
+print(bool(0.5))
+print(bool('0'))
+print(bool('True'))
+print(bool('False'))
+print(bool(False))
+print(bool('any random thing'))
