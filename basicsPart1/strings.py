9006333696d30fbeb57d3980243d88090c4e4e3c
Andre Jarboe <andrejarboe@gmail.com>
2022-09-01 16:06:22 -0400

    #Escape sequence

--- basicsPart1/strings.py
+++ basicsPart1/strings.py
@@ -8,4 +8,18 @@ WOW
 0 0
 ---
 """
+
+long2 = '''
+BIG
+NOW
+HOW
+POW
+'''
 print(long_string)
+print(long2)
+
+#Escape sequence
+weather = "\t It's \"kind of\" sunny \n hope you have a god day!"
+
+print(weather)
+
