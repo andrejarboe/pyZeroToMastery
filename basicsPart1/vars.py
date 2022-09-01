c7aa61058ccb63478443f242049a8eb3252d102d
Andre Jarboe <andrejarboe@gmail.com>
2022-09-01 15:44:52 -0400

    vars

---
+++ basicsPart1/vars.py
@@ -0,0 +1,11 @@
+# vars notes:
+# snake_case
+# start with lowercase or underscore
+# use letter, numbers, and underscores,
+# case sensitive
+# dont overwrite keywords
+
+user_IQ = 190
+
+print(user_IQ)
+
