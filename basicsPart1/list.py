44361ce2b6ed5c7fb0fedb1b88fa18b96f94881c
andrejarboe <andrejarboe@gmail.com>
2022-09-01 17:44:01 -0400

    list and start matrix file

---
+++ basicsPart1/list.py
@@ -0,0 +1,26 @@
+'''
+list isn a ordered sequence of objects
+bruh its an array lol 
+they do have their own arry though 
+
+list are mutable 
+
+#Data structures
+    * a container around your data
+'''
+
+li = [1, 2.5, "hello", True]
+print(li)
+
+amazon_cart = [
+    "laptop",
+    "book",
+    "phone",
+    "pen",
+    "key"
+   ]
+# start:stop:step over
+print(amazon_cart[0])
+print(amazon_cart[::-1])
+print(amazon_cart[0:3])
+print(amazon_cart)
